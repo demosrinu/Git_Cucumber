84c865821f9bcf0ccd7b9982bada94a598df99bd
demosrinu <srinu.sandavena245@gmail.com>
2023-06-16 00:49:00 +0530

    git push from master - eclipse

--- src/test/java/pageObjects/Offerspage.java
+++ src/test/java/pageObjects/Offerspage.java
@@ -11,7 +11,7 @@ public class Offerspage {
 	By Productname = By.cssSelector("tr td:nth-child(1)");
 	By searching = By.xpath("//input[@type='search']");//dummy
 	
-	
+	//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 - git token
 	public Offerspage(WebDriver driver) {
 		this.driver=driver;
 	}
